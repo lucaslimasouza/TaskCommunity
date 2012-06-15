342aecd4cd45524f4548a6c8ade70c1fd51792c8
Lucas Lima de Souza <lucaslima4p@gmail.com>
2012-06-15 17:43:06 -0300

    integration test add listof task =D running

--- spec/requests/lists_spec.rb
+++ spec/requests/lists_spec.rb
@@ -4,41 +4,30 @@ include Warden::Test::Helpers
 
 describe "Lists" do
 
-  describe "POST /lists" do
-
-	before(:each) do
-		Warden.test_mode!
-		@user = User.new(:email => 'lucaslima4p@gmail.com', :password => '123456')
-		login_as @user, :scope => :user
-	end
-
-	#only to test if I can login here
-	it "sign_in" do
-		visit user_session_path
-		fill_in 'Email', :with => @user.email
-		fill_in 'Password', :with => @user.password
-		click_button 'Sign in'
-		save_and_open_page
-	end
-
-	it "create List" do
-		visit new_list_path
-		fill_in('Name List', :with => 'Miniapp')
-	        choose('Public')
-	        fill_in('Name Task', :with => "nested attributes")
-	        click_button 'Submit'
-	        page.should have_content("List was successfully created")
-		save_and_open_page
-	end
-
-	after(:each) do
-	  Warden.test_reset!
-	end
+  before(:each) do
+	Warden.test_mode!
+	@user = User.new(:email => 'lucaslima4p@gmail.com', :password => '123456')
 
   end
 
-	def sign_in
+  describe "POST /lists" do
 
+	it "create List" do
+		login_as(@user, :scope => :user) do
+			visit new_list_path
+			fill_in('Name List', :with => 'Miniapp')
+			choose('Public')
+			fill_in('Name Task', :with => "nested attributes")
+			click_button 'Submit'
+			page.should have_content("List was successfully created")
+			current_path.should == root_path
+		end
 	end
+  end
+
+  after(:all) do
+	 Warden.test_reset!
+  end
+
 
 end
